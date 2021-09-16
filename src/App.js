81b8e9eab6e58ac19a6a0b89b9869f671e8f34d8
Paul Ezaga <paulezaga@gmail.com>
2021-09-16 11:17:08 +0200

    Pagination Does not seem to Work Well

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react"
-import { Admin, Resource } from 'react-admin'
-import restProvider from 'ra-data-simple-rest'
+import { Admin, Resource, fetchUtils } from 'react-admin'
+import simpleRestProvider from 'ra-data-simple-rest'
 import AuthorList  from './components/AuthorList'
 import AuthorCreate from "./components/AuthorCreate"
 import AuthorEdit from "./components/AuthorEdit"
@@ -8,8 +8,18 @@ import BookList  from './components/BookList'
 import BookCreate from "./components/BookCreate"
 import BookEdit from "./components/BookEdit"
 
+const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }   
+    options.headers.set('Access-Control-Expose-Headers','Content-Range') 
+    return fetchUtils.fetchJson(url, options);
+};
+const dataProvider = simpleRestProvider('http://localhost:3000', httpClient);
+
+
 const App = () => (
-    <Admin dataProvider={ restProvider('http://localhost:3000/') }>
+    <Admin dataProvider={dataProvider} >
         <Resource name="authors"  list={AuthorList}  create={AuthorCreate} edit={AuthorEdit} />
         <Resource name="books"  list={BookList}  create={BookCreate} edit={BookEdit} />
     </Admin>
